07a1c25aef78df69725a16e469b146f27eb909a7
Nathan Laundry <nathanlaundry@rogers.com>
2021-07-14 21:36:24 -0400

    Added styles for the burger menu
    Co-authored-by: Nausher Rao <SherRao15@gmail.com>
    Co-authored-by: Riley Adams <Royce-33@users.noreply.github.com>

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -14,6 +14,50 @@ const Container = styled(FlexRow)`
     justify-content: flex-end;
 `;
 
+var styles = {
+    bmBurgerButton: {
+        position: "fixed",
+        width: "36px",
+        height: "30px",
+        left: "36px",
+        top: "36px"
+    },
+    bmBurgerBars: {
+        background: "white"
+    },
+    bmBurgerBarsHover: {
+        background: "#a90000"
+    },
+    bmCrossButton: {
+        height: "24px",
+        width: "24px"
+    },
+    bmCross: {
+        background: "#bdc3c7"
+    },
+    bmMenuWrap: {
+        position: "fixed",
+        height: "100%"
+    },
+    bmMenu: {
+        background: "#373a47",
+        padding: "2.5em 1.5em 0",
+        fontSize: "1.15em"
+    },
+    bmMorphShape: {
+        fill: "#373a47"
+    },
+    bmItemList: {
+        color: "#b8b7ad",
+        padding: "0.8em"
+    },
+    bmItem: {
+        // display: "inline-block"
+    },
+    bmOverlay: {
+        background: "rgba(0, 0, 0, 0.3)"
+    }
+};
 
 function Header() {
     const isMobile = useMediaPredicate("(max-width: 1000px)");
@@ -21,12 +65,11 @@ function Header() {
         <>
             {
                 isMobile ?
-                    <Menu width={"40%"}>
+                    <Menu styles={styles} >
                         {
                             pages.map((page) => (
-                                <a key={page} className="menu-item" text={page} href={`/${page}`}>{page}</a>
+                                <a key={page} className="bmItem" text={page} href={`/${page}`}>{page}</a>
                             ))
-
                         }
                     </Menu>
                     :

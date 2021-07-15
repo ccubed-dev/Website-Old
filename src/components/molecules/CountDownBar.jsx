2ccdfc530776a5d181c282e6b8fe19bc80af8e03
Nausher Rao <SherRao15@gmail.com>
2021-07-14 20:58:51 -0400

    Add a functioning countdown timer that actually counts down to CubedCon
    Delete temp.js since all the code got moved to CountDownBar.jsx

--- src/components/molecules/CountDownBar.jsx
+++ src/components/molecules/CountDownBar.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import {Button,Card,FlexRow} from "@atoms";
+import {Button, Card, FlexRow, FlexColumn} from "@atoms";
 
 const Container = styled(Card)`
 	display: flex;
 	flex-direction: column;
 	padding: 2em;
 	width: 60%;
-    /* height: 20%; */
 
     @media screen and (max-width: 1000px) {
         width:80%;
@@ -16,12 +15,19 @@ const Container = styled(Card)`
     //centers
     margin-left: auto;
     margin-right: auto;
+
 `;
 
 const CountDownContainer = styled(FlexRow)`
     justify-content: space-between;
     padding-left: 10%;
     padding-right: 10%;
+    
+`;
+
+const NumberContainer = styled(FlexColumn)`
+    align-items: center;
+
 `;
 
 const Header = styled.h1`
@@ -33,34 +39,88 @@ const SubHeader = styled.h1`
     font-size: 1.5em;
     color: white;
 `;
+
 const ButtonContainer = styled(FlexRow)`
-    align-items: center;
-    justify-items: center;
-    text-align: center;
     width: 100%;
+    text-align: center;
+    display: inline;
+
 `;
 
-export default function MetricsBar() {
+function MetricsBar() {
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(getTimeLeft());
+        });
+
+        return () => clearTimeout(timer);
+    });
+
     return (
         <Container>
             <CountDownContainer>
-                <Header>10</Header>
-                <Header>:</Header>
-                <Header>00</Header>
-                <Header>:</Header>
-                <Header>00</Header>
-                <Header>:</Header>
-                <Header>00</Header>
+                <NumberContainer>
+                    <Header>{timeLeft.days}</Header>
+                    <SubHeader>Days</SubHeader>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>:</Header>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>{timeLeft.hours}</Header>
+                    <SubHeader>Hours</SubHeader>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>:</Header>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>{timeLeft.minutes}</Header>
+                    <SubHeader>Minutes</SubHeader>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>:</Header>
+                </NumberContainer>
+
+                <NumberContainer>
+                    <Header>{timeLeft.seconds}</Header>
+                    <SubHeader>Seconds</SubHeader>
+                </NumberContainer>
             </CountDownContainer>
-            <CountDownContainer>
-                <SubHeader>Days</SubHeader>
-                <SubHeader>Hours</SubHeader>
-                <SubHeader>Minutes</SubHeader>
-                <SubHeader>Seconds</SubHeader>
-            </CountDownContainer>
+
             <ButtonContainer>
                 <Button>Register</Button>
             </ButtonContainer>
         </Container>
     );
+}
+
+export default MetricsBar;
+
+const NEXT_EVENT_YEAR = 2021;
+const NEXT_EVENT_MONTH = 8;
+const NEXT_EVENT_DAY = 25;
+const NEXT_EVENT_HOUR = 0;
+
+const NEXT_EVENT_DATE = new Date(NEXT_EVENT_YEAR, NEXT_EVENT_MONTH, NEXT_EVENT_DAY, NEXT_EVENT_HOUR, 0, 0, 0);
+
+function getTimeLeft() {
+    let timeLeft = new Date(NEXT_EVENT_DATE - new Date());
+    let days = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
+    timeLeft %= (24 * 60 * 60 * 1000);
+
+    let hours = Math.floor(timeLeft / (60 * 60 * 1000));
+    timeLeft %= (60 * 60 * 1000);
+
+    let minutes = Math.floor(timeLeft / (60 * 1000));
+    timeLeft %= (60 * 1000);
+
+    let seconds = Math.floor(timeLeft / 1000);
+    return { "days": days, "hours": hours, "minutes": minutes, "seconds": seconds };
+
 }
